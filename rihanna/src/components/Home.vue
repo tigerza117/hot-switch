00d501e2fdf3a2a5566132597712d76ff01585f2
tigerza117 <noppawanza1171@gmail.com>
2021-05-07 00:56:47 +0700

    feat: 🎸 responsive support

--- rihanna/src/components/Home.vue
+++ rihanna/src/components/Home.vue
@@ -1,30 +1,41 @@
 <template>
   <div class="home min-w-full md:flex my-auto p-8 space-y-16">
-    <div class="w-full md:w-1/2 space-y-4">
+    <div class="w-full md:w-1/2 space-y-6">
       <div class="logo mx-auto md:mx-0">
         <img alt="logo" src="../assets/logo.png" />
       </div>
       <div
-        class="title text-6xl font-bold text-center md:text-left xl:text-9xl"
+        class="title text-6xl font-bold text-center md:text-left md:text-7xl lg:text-8xl xl:text-9xl"
       >
         HOT SWITCH
       </div>
-      <p class="description break-words md:w-1/2">
+      <p class="description break-words lg:w-1/2">
         A new way to control your life. Power of controlling your electronic
         devices in your phone, anywhere, that have an internet.
       </p>
-      <div class="links space-x-2 flex justify-center md:justify-start">
-        <a class="flex github items-center space-x-2">
+      <div
+        class="links space-x-4 lg:space-x-2 flex justify-center md:justify-start"
+      >
+        <a
+          class="flex github items-center space-x-2 rounded-2xl p-4 lg:p-2 md:border-4"
+          href="https://github.com/tigerza117/hot-switch"
+        >
           <img alt="github" src="../assets/github.svg" />
-          <p>Github</p>
+          <p class="hidden lg:inline">Github</p>
         </a>
-        <a class="flex document items-center space-x-2">
+        <router-link
+          to="/document"
+          class="flex document items-center space-x-2 rounded-2xl p-4 lg:p-2 md:border-4"
+        >
           <img alt="document" src="../assets/document.svg" />
-          <p>Document</p>
-        </a>
-        <a class="flex dashboard items-center space-x-2">
+          <p class="hidden lg:inline">Document</p>
+        </router-link>
+        <a
+          href="http://103.91.205.130"
+          class="flex dashboard items-center space-x-2 rounded-2xl p-4 lg:p-2 md:border-4"
+        >
           <img alt="dashboard" src="../assets/dashboard.svg" />
-          <p>Dashboard</p>
+          <p class="hidden lg:inline">Dashboard</p>
         </a>
       </div>
     </div>
@@ -46,17 +57,13 @@ export default {
 .home .logo {
   width: 100px;
 }
-.links > * {
-  border-radius: 30px;
-  padding: 5px 10px;
-}
 .links .github {
-  border: 4px solid #7d2dff;
+  border-color: #7d2dff;
 }
 .links .document {
-  border: 4px solid black;
+  border-color: black;
 }
 .links .dashboard {
-  border: 4px solid #2d9aff;
+  border-color: #2d9aff;
 }
 </style>
--- rihanna/src/components/Navbar.vue
+++ rihanna/src/components/Navbar.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="navbar">
-    <div class="px-14 py-6 flex justify-between container mx-auto">
-      <div>ComPro // MiniProject</div>
-      <div>
-        <div class="hidden md:flex space-x-14">
-          <router-link to="/">Home</router-link>
-          <a href="https://github.com/tigerza117/hot-switch">Github</a>
-          <router-link to="/document">Document</router-link>
-          <router-link to="/team">Team</router-link>
+    <div class="px-14 py-6 md:flex justify-between container mx-auto">
+      <div class="flex w-full justify-between">
+        <router-link to="/">ComPro // MiniProject</router-link>
+        <div class="md:hidden">
+          <i class="fas fa-bars" @click="show = !show" />
         </div>
+      </div>
+      <div class="space-x-14 md:flex my-8 md:m-0" v-if="collapse">
+        <router-link to="/">Home</router-link>
+        <a href="https://github.com/tigerza117/hot-switch">Github</a>
+        <router-link to="/document">Document</router-link>
+        <router-link to="/team">Team</router-link>
       </div>
     </div>
   </div>
@@ -17,6 +20,16 @@
 <script>
 export default {
   name: 'Navbar',
+  data() {
+    return {
+      show: false,
+    };
+  },
+  computed: {
+    collapse() {
+      return window.innerWidth >= 768 || (this.show && window.innerWidth < 768);
+    },
+  },
 };
 </script>
 
