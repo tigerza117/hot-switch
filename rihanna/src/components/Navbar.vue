119c1df2ceb0c1c2cbf36cd4ef18764966d3e2cb
tigerza117 <noppawanza1171@gmail.com>
2021-05-07 04:21:49 +0700

    feat: 🎸 add dashboard navbar

--- rihanna/src/components/Navbar.vue
+++ rihanna/src/components/Navbar.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="navbar">
-    <div class="px-8 py-6 md:flex justify-between container mx-auto">
+    <div class="px-8 py-6 lg:flex justify-between container mx-auto">
       <div class="flex w-full justify-between">
         <router-link class="py-2" to="/">ComPro // MiniProject</router-link>
-        <div class="md:hidden">
+        <div class="lg:hidden">
           <i class="fas fa-bars" @click="show = !show" />
         </div>
       </div>
-      <div class="md:space-x-14 md:flex my-8 md:m-0" v-if="collapse">
+      <div class="lg:space-x-14 lg:flex my-4 lg:m-0" v-if="collapse">
         <router-link class="block py-2" to="/">Home</router-link>
         <a class="block py-2" href="https://github.com/tigerza117/hot-switch">
           Github
         </a>
         <router-link class="block py-2" to="/document">Document</router-link>
         <router-link class="block py-2" to="/team">Team</router-link>
+
+        <a class="block py-2" href="http://103.91.205.130"> Dashboard </a>
       </div>
     </div>
   </div>
@@ -29,7 +31,7 @@ export default {
   },
   computed: {
     collapse() {
-      return window.innerWidth >= 768 || (this.show && window.innerWidth < 768);
+      return window.innerWidth > 992 || (this.show && window.innerWidth <= 992);
     },
   },
 };
